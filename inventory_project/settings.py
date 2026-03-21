1ae9923bae607ff5379b18aefa3e62512af8fb93
sammohan <sammohan693@gmail.com>
2026-03-21 13:12:09 +0530

    add whitenoise

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -106,9 +106,16 @@ AUTHENTICATION_BACKENDS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-import dj_database_url
 import os
+import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
