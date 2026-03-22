33eae83383353aab46cc9361049b5e230bc457e9
sammohan <sammohan693@gmail.com>
2026-03-22 20:08:25 +0530

    fix media

--- inventory_project/settings.py
+++ inventory_project/settings.py
@@ -107,3 +107,6 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- inventory_project/urls.py
+++ inventory_project/urls.py
@@ -21,3 +21,13 @@ urlpatterns = [
 
 # 🔥 MEDIA FILES
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('inventory.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
